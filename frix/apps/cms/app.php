6cf10daca75905ecc228488d8b51887c556e2b37
Enrico <enrico.luz@corp.globo.com>
2010-06-19 14:03:08 -0300

    Added method to get root pages.

--- frix/apps/cms/app.php
+++ frix/apps/cms/app.php
@@ -1,6 +1,11 @@
 <?
 class CmsApp extends App {
 	
+	function get_root_pages () {
+		$this->load_model('CmsPage');
+		return CmsPage::meta()->filter(array('visible' => true))->all(array('parent' => null));
+	}
+
 	function get_child_pages ($slug) {
 		
 		$this->load_model('CmsPage');
